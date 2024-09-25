49f85311942b82a05001fd9f26ae22023992ca7f
Dheeraj <dheerajkum838@gmail.com>
2024-09-25 19:15:32 +0500

    febicon change

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import { WavyBackgroundDemo } from "@/components/bg";
 
 export default function Home() {
   return (
-    <div className="bg-black h-screen overflow-hidden"> 
-    <WavyBackgroundDemo/>\
+    <div className="bg-black h-screen overflow-hidden">
+      <WavyBackgroundDemo />
     </div>
   );
 }
